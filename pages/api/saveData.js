e625afbe50c7939ddc8e96701877c1eb51324298
hulff <ph2717693@gmail.com>
2023-06-11 19:27:31 -0300

    structure data

--- pages/api/saveData.js
+++ pages/api/saveData.js
@@ -2,12 +2,17 @@ import { writeData, getData } from "../../services/firebase";
 
 function saveData(req, res) {
   const date = new Date();
-  let data = { ...req.body };
+  let data = {
+    temp:req.body.temp,
+    vocs:req.body.vocs,
+    co2:req.body.co2
+
+  }
   console.log(data);
 
   // res.setHeader("Cache-Control", "s-maxage=10, stale-while-revalidate");
 
-  writeData(data.code, date, data.temp);
+  writeData(data.code, date, data);
 
   res.json({
     horario: date,
