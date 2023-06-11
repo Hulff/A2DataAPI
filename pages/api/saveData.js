449cd259c494db048a9722cfce036b3e105fe883
hulff <ph2717693@gmail.com>
2023-06-11 18:34:02 -0300

    add post route

---
+++ pages/api/saveData.js
@@ -0,0 +1,16 @@
+import { writeData, getData } from "../services/firebase"
+
+function saveData(req, res) {
+  const date = new Date();
+  let data = { ...req.body };
+  console.log(data);
+
+  res.setHeader("Cache-Control", "s-maxage=10, stale-while-revalidate");
+
+  writeData(data.code,date, data.data);
+
+  res.json({
+    horario: date,
+  });
+}
+export default saveData;
---
+++ pages/services/firebase.js
@@ -0,0 +1,28 @@
+import { initializeApp } from "firebase/app";
+import { getDatabase, ref, set, remove, get, child } from "firebase/database";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAkV7_nu0LT2Ya8GQzsSYP5mITLGKEuCTo",
+    authDomain: "a2dataapi.firebaseapp.com",
+    projectId: "a2dataapi",
+    storageBucket: "a2dataapi.appspot.com",
+    messagingSenderId: "17389564071",
+    appId: "1:17389564071:web:f227c75e8bbfe1be5beb82",
+    measurementId: "G-YRM4GW93B5"
+  };
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+
+export function writeData(code,time,data) {
+  set(ref(database, `devices/${code}/${time}`), data);
+}
+export async function getData(code) {
+  const snapshot = await get(child(ref(database), `devices/${code}`));
+  if (snapshot.exists()) {
+    let data = snapshot.val();
+    return data;
+  } else {
+    console.log("No data available");
+    return null;
+  }
+}
