5d47da273f016758a0e42a1f4e857f41376a52b7
hulff <ph2717693@gmail.com>
2023-06-11 19:43:56 -0300

    bug?

--- pages/api/saveData.js
+++ pages/api/saveData.js
@@ -3,11 +3,10 @@ import { writeData, getData } from "../../services/firebase";
 function saveData(req, res) {
   const date = new Date();
   let data = {
-    temp:req.body.temp,
-    vocs:req.body.vocs,
-    co2:req.body.co2
-
-  }
+    temp: req.body.temp,
+    vocs: req.body.vocs,
+    co2: req.body.co2,
+  };
   console.log(data);
 
   // res.setHeader("Cache-Control", "s-maxage=10, stale-while-revalidate");
