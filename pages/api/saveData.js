8e6acfcea92394c900796009fcf8234ca3fdcedb
hulff <ph2717693@gmail.com>
2023-06-12 11:51:28 -0300

    b

--- pages/api/saveData.js
+++ pages/api/saveData.js
@@ -1,13 +1,8 @@
-import { writeData, getData } from "../../services/firebase";
+import { writeData} from "../../services/firebase";
 
 function saveData(req, res) {
   const currentDate = new Date();
   const adjustedDate = new Date(currentDate.getTime() + -180 * 60000);
-  const locale = "pt-BR";
-  const formattedDate = adjustedDate.toLocaleString(locale);
-
-  console.log("Data:", formattedDate);
-
   let data = {
     temp: req.body.temp,
     vocs: req.body.vocs,
@@ -15,9 +10,8 @@ function saveData(req, res) {
   };
   console.log(data);
   writeData(req.body.code, adjustedDate, data);
-  // res.setHeader("Cache-Control", "s-maxage=10, stale-while-revalidate");
   res.json({
-    horario: formattedDate,
+    horario: adjustedDate,
   });
 }
 export default saveData;
