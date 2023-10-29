516b674ddb8805b5678a8a0907405540300e38f9
Hugo <ph2717693@gmail.com>
2023-10-29 19:01:01 -0300

    help

--- services/firebase.js
+++ services/firebase.js
@@ -5,13 +5,13 @@ import { getFirestore, doc, setDoc } from "firebase/firestore";
 // Add a new document in collection "cities"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAkV7_nu0LT2Ya8GQzsSYP5mITLGKEuCTo",
-  authDomain: "a2dataapi.firebaseapp.com",
-  projectId: "a2dataapi",
-  storageBucket: "a2dataapi.appspot.com",
-  messagingSenderId: "17389564071",
-  appId: "1:17389564071:web:f227c75e8bbfe1be5beb82",
-  measurementId: "G-YRM4GW93B5",
+  apiKey: "AIzaSyBdLJtpVObKArm9HKgl1lsqgwtNy8oUtVE",
+  authDomain: "alinhautossys.firebaseapp.com",
+  projectId: "alinhautossys",
+  storageBucket: "alinhautossys.appspot.com",
+  messagingSenderId: "239626613809",
+  appId: "1:239626613809:web:1f66de3c4b6d65353e1110",
+  measurementId: "G-HXHR9P5BK4"
 };
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app)
